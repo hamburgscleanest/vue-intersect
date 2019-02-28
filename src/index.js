c6825cf796911f693f8a58387f80d387a646294f
Chroma91 <chroma91@gmail.com>
2019-02-28 20:57:01 +0100

    fix ssr issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-const warn = (msg) => {
+const warn = msg => {
   if (!Vue.config.silent) {
     console.warn(msg)
   }
@@ -16,7 +16,7 @@ export default {
       default: () => [0, 0.2]
     },
     root: {
-      type: HTMLElement,
+      type: typeof HTMLElement !== 'undefined' ? HTMLElement : Object,
       required: false,
       default: () => null
     },
@@ -26,27 +26,33 @@ export default {
       default: () => '0px 0px 0px 0px'
     }
   },
-  created () {
-    this.observer = new IntersectionObserver((entries) => {
-      if (!entries[0].isIntersecting) {
-        this.$emit('leave', [entries[0]])
-      } else {
-        this.$emit('enter', [entries[0]])
-      }
-
-      this.$emit('change', [entries[0]])
-    }, {
-      threshold: this.threshold,
-      root: this.root,
-      rootMargin: this.rootMargin
-    })
-  },
   mounted () {
+    this.observer = new IntersectionObserver(
+      entries => {
+        if (!entries[0].isIntersecting) {
+          this.$emit('leave', [entries[0]])
+        } else {
+          this.$emit('enter', [entries[0]])
+        }
+
+        this.$emit('change', [entries[0]])
+      },
+      {
+        threshold: this.threshold,
+        root: this.root,
+        rootMargin: this.rootMargin
+      }
+    )
+
     this.$nextTick(() => {
       if (this.$slots.default && this.$slots.default.length > 1) {
-        warn('[VueIntersect] You may only wrap one element in a <intersect> component.')
+        warn(
+          '[VueIntersect] You may only wrap one element in a <intersect> component.'
+        )
       } else if (!this.$slots.default || this.$slots.default.length < 1) {
-        warn('[VueIntersect] You must have one child inside a <intersect> component.')
+        warn(
+          '[VueIntersect] You must have one child inside a <intersect> component.'
+        )
         return
       }
 
